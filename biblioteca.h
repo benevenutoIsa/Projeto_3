82e5fe3831c68e06d6805048497cf391f74b842a
benevenutoIsa <142352312+benevenutoIsa@users.noreply.github.com>
2023-11-10 03:47:30 +0000

    adcionando as assiaturas de todas as funções, tanto as antigas do projeto 1 quanto as novas usei o enum para ficar mais facil na hora de aplicar nas outras funções, por isso esta declarado no inicio junto com a struct.

---
+++ biblioteca.h
@@ -0,0 +1,30 @@
+typedef enum {
+    COMPLETO,
+    EM_ANDAMENTO,
+    NAO_INICIADO
+} EstadoTarefa;
+
+typedef struct {
+    int prioridade;
+    char descricao[301];
+    char categoria[101];
+    EstadoTarefa estado;
+} Tarefa;
+
+//essas funções foram implementadas no projeto 1 mas fiz algumas alterações
+void menu();
+void escreverTarefa(Tarefa *tarefa);
+void salvarStruct(const Tarefa *tarefa, int n, const char *arquivoBin);
+void lerStruct(Tarefa *tarefa, char *arquivoBin);
+void listarStruct(const Tarefa *tarefa, int n);
+void excluir(Tarefa *tarefa, int indice, int *n);
+
+//já essas funções são novas especificadamente do projjeto 3
+void alterarTarefas(Tarefa *tarefa, int n);
+void filtrarPrioridade(const Tarefa *tarefa, int n, int prioridade);
+void filtrarEstado(const Tarefa *tarefa, int n, EstadoTarefa estado);
+void filtrarCategoria(const Tarefa *tarefa, int n, const char *categoria);
+void filtrarPrioridadeCategoria(const Tarefa *tarefa, int n, int prioridade, const char *categoria);
+void exportarPrioridade(const Tarefa *tarefa, int n, int prioridade, const char *nomeArquivo);
+void exportarCategoria(const Tarefa *tarefa, int n, const char *categoria, const char *nomeArquivo);
+void exportarPrioridadeCategoria(const Tarefa *tarefa, int n, int prioridade, const char *categoria, const char *nomeArquivo);
